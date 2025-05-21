75e2545cc885031ada3e899cb4e76671b057d173
bingchillin <92374981+bingchillin@users.noreply.github.com>
2025-05-21 13:59:19 +0200

    Added sign up

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -4,10 +4,17 @@ import { LocalAuthGuard } from './local-auth.guard';
 import { Request } from 'express';
 import { AuthDocs } from 'src/api/swagger/auth.docs';
 import { LoginDto } from './dto/login.dto';
+import { SignupDto } from './dto/signup.dto';
 
 @Controller('auth/user')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
+
+    @Post('signup')
+    @AuthDocs.signup()
+    async signup(@Body() signupDto: SignupDto) {
+        return this.authService.signup(signupDto);
+    }
 
     @Post('login')
     @UseGuards(LocalAuthGuard)
