9850ea30313e1c7b75dc64b581320eebd32c9227
U-Gabriel <gabrielarie2000@gmail.com>
2025-05-23 00:05:01 +0200

    ADD home + login + signup

---
+++ app/lib/ui/pages/advise_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AdvisePage extends StatelessWidget {
+  const AdvisePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('💬 Advise Page', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
