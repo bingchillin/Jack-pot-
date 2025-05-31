895fe2ebf6ca15a006f37680ea5fefa64fca40d0
bingchillin <mohamedelfakharany992@gmail.com>
2025-05-31 11:24:33 +0200

    Backoffice - Finished Plant type

---
+++ backoffice/src/utils/api/enum.ts
@@ -0,0 +1,15 @@
+const seasons = {
+    Spring: "Spring",
+    Summer: "Summer",
+    Autumn: "Autumn",
+    Winter: "Winter",
+}
+
+const seasonColors = {
+    Spring: "orange",
+    Summer: "red",
+    Autumn: "yellow",
+    Winter: "blue",
+}
+
+export { seasons, seasonColors };
