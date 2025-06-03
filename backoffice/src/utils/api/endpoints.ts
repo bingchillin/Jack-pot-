c66132ecd6f4931a2fec5a903205465f4491152d
bingchillin <92374981+bingchillin@users.noreply.github.com>
2025-06-03 15:13:00 +0200

    Added Object entity, needs CategoryType dependency

--- backoffice/src/utils/api/endpoints.ts
+++ backoffice/src/utils/api/endpoints.ts
@@ -2,6 +2,7 @@ export const getResourceEndpoint = (resource: string): string => {
   const resourceMappings: { [key: string]: string } = {
 
 
+
     persons: "person",
     "plant-types": "plant-type",
     "event-parties": "event-party",
@@ -9,6 +10,7 @@ export const getResourceEndpoint = (resource: string): string => {
     "parameter-types": "parameter-type",
     "plants": "plants",
     "roles": "role",
+  "objects": "object",
 };
 
   return resourceMappings[resource] || resource;
