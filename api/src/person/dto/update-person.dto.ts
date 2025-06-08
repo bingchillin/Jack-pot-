a846811c93c5009fcec9d5958ef8cac8e1f356a1
bingchillin <92374981+bingchillin@users.noreply.github.com>
2025-06-08 22:43:58 +0200

    Fixed several auth requests

--- api/src/person/dto/update-person.dto.ts
+++ api/src/person/dto/update-person.dto.ts
@@ -1,6 +1,7 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreatePersonDto } from './create-person.dto';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString, IsDate, IsNumber, MaxLength } from 'class-validator';
 
 export class UpdatePersonDto extends PartialType(CreatePersonDto) {
     @ApiProperty({ 
@@ -11,4 +12,21 @@ export class UpdatePersonDto extends PartialType(CreatePersonDto) {
         format: 'password'
     })
     password?: string;
+
+    @IsOptional()
+    @IsString()
+    verificationCode?: string;
+
+    @IsOptional()
+    @IsDate()
+    verificationCodeExpires?: Date;
+
+    @IsOptional()
+    @IsNumber()
+    resetTokenVersion?: number;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(50)
+    numberPhone?: string;
 } 
