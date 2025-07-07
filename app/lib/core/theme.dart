918609ebb0db97c5a74d983a7d79df35b5a65492
bingchillin <92374981+bingchillin@users.noreply.github.com>
2025-07-07 04:33:51 +0200

    Added font to app bar title

--- app/lib/core/theme.dart
+++ app/lib/core/theme.dart
@@ -3,4 +3,13 @@ import 'package:flutter/material.dart';
 final ThemeData appTheme = ThemeData(
   colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
   useMaterial3: true,
+  
+  // AppBar theme for navbar pages
+  appBarTheme: const AppBarTheme(
+    titleTextStyle: TextStyle(
+      fontFamily: '04B_30__',
+      fontSize: 20,
+      color: Colors.black87,
+    ),
+  ),
 );
