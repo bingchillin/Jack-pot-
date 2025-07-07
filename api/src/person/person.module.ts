369fc096f2d7fe4e2ecfd4be404bf2d6f92c87ce
bingchillin <92374981+bingchillin@users.noreply.github.com>
2025-07-07 16:58:06 +0200

    Added comments endpoint

--- api/src/person/person.module.ts
+++ api/src/person/person.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonService } from './person.service';
 import { PersonController } from './person.controller';
 import { Person } from './entities/person.entity';
+import { Comment } from '../comment/entities/comment.entity';
+import { CommentLike } from '../comment/entities/comment-like.entity';
 import { RoleModule } from '../role/role.module';
 import { StripeModule } from '../stripe/stripe.module';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Person]),
+        TypeOrmModule.forFeature([Person, Comment, CommentLike]),
         RoleModule,
         forwardRef(() => StripeModule)
     ],
